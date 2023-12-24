cca04934064d4d1a7bc0e35c2b055ae682dac47d
slamensd <58567346+slamensd@users.noreply.github.com>
2023-12-24 18:25:43 +0000

    go

--- script.js
+++ script.js
@@ -1,71 +1,58 @@
-// script.js
 document.addEventListener('DOMContentLoaded', () => {
   const cameraStream = document.getElementById('cameraStream');
   const snapshotCanvas = document.getElementById('snapshot');
   const qrCanvas = document.getElementById('qrCanvas');
   const captureBtn = document.getElementById('captureBtn');
   const scanBtn = document.getElementById('scanBtn');
+  const startCameraBtn = document.getElementById('startCameraBtn');
   const screens = document.querySelectorAll('.screen');
-  const closeIcons = document.querySelectorAll('.close-icon');
 
-  // Function to show only the active screen
   function showScreen(screenIndex) {
-    screens.forEach((screen, index) => {
-      screen.style.display = index === screenIndex ? 'block' : 'none';
-    });
+      screens.forEach((screen, index) => {
+          screen.style.display = index === screenIndex ? 'block' : 'none';
+      });
   }
 
-  // Close icons event
-  closeIcons.forEach((icon, index) => {
-    icon.addEventListener('click', () => {
-      showScreen(index === 0 ? null : index - 1); // Go back to the previous screen or hide all if it's the first screen
-    });
-  });
+  function startCamera() {
+      navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } })
+          .then(stream => {
+              cameraStream.srcObject = stream;
+              cameraStream.play();
+              showScreen(0);
+          })
+          .catch(err => {
+              console.error('Camera access error:', err);
+              alert('Error accessing the camera. Please make sure you have given the necessary permissions.');
+          });
+  }
 
-  // Camera stream setup
-  navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } })
-    .then(stream => {
-      cameraStream.srcObject = stream;
-      cameraStream.play();
-      showScreen(0); // Show the first screen
-    })
-    .catch(err => {
-      console.error('An error occurred: ' + err);
-      alert('Error accessing the camera. Please make sure you have given the necessary permissions.');
-    });
+  startCameraBtn.addEventListener('click', startCamera);
 
-  // Capture Button Event
   captureBtn.addEventListener('click', () => {
-    const context = snapshotCanvas.getContext('2d');
-    context.drawImage(cameraStream, 0, 0, snapshotCanvas.width, snapshotCanvas.height);
-    showScreen(1); // Show the second screen
+      const context = snapshotCanvas.getContext('2d');
+      context.drawImage(cameraStream, 0, 0, snapshotCanvas.width, snapshotCanvas.height);
+      showScreen(1);
   });
 
-  // QR Code Scan Event
   scanBtn.addEventListener('click', () => {
-    const context = qrCanvas.getContext('2d');
-    context.drawImage(cameraStream, 0, 0, qrCanvas.width, qrCanvas.height);
-
-    // Get image data from canvas
-    const imageData = context.getImageData(0, 0, qrCanvas.width, qrCanvas.height);
-
-    // Scan for QR Code
-    const code = jsQR(imageData.data, imageData.width, imageData.height, {
-      inversionAttempts: 'dontInvert',
-    });
-
-    if (code) {
-      console.log(`Found QR code: ${code.data}`);
-      showScreen(2); // Show the third screen
-      startRewardAnimation();
-    } else {
-      console.error('No QR code found.');
-      alert('No QR code found. Please try again.');
-    }
+      const context = qrCanvas.getContext('2d');
+      context.drawImage(cameraStream, 0, 0, qrCanvas.width, qrCanvas.height);
+      const imageData = context.getImageData(0, 0, qrCanvas.width, qrCanvas.height);
+      
+      // Scan for QR Code using jsQR
+      const code = jsQR(imageData.data, imageData.width, imageData.height);
+      if (code) {
+          console.log(`Found QR code: ${code.data}`);
+          showScreen(2); // Show the reward screen
+          startRewardAnimation();
+      } else {
+          console.log("No QR code found.");
+          alert("No QR code found. Please try again.");
+      }
   });
 
   function startRewardAnimation() {
-    const coinAnimation = document.getElementById('coinAnimation');
-    coinAnimation.classList.add('spin');
+      // Placeholder for reward animation logic
+      console.log("Starting reward animation...");
   }
 });
