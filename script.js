048ee0c00de2ce2a26a98a427089f6983e4acc16
slamensd <58567346+slamensd@users.noreply.github.com>
2023-12-25 19:09:14 +0000

    ff

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
-  // DOM elements
   const captureBtn = document.getElementById('capture-btn');
   const scanBtn = document.getElementById('scan-btn');
   const homeScreen = document.getElementById('home-screen');
@@ -8,27 +7,21 @@ document.addEventListener("DOMContentLoaded", function() {
   const rewardScreen = document.getElementById('reward-screen');
   let videoStream = null;
   let videoElement = null;
-  let qrScanner = null;
 
-  // Navigate to Camera Screen
   captureBtn.addEventListener('click', function() {
       navigateToScreen(cameraScreen);
       initializeCamera();
   });
 
-  // Initialize Camera
   function initializeCamera() {
       if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
           const constraints = { video: { facingMode: "environment" } };
-
           navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
               videoStream = stream;
               videoElement = document.createElement('video');
               videoElement.srcObject = stream;
               videoElement.play();
               cameraScreen.appendChild(videoElement);
-
-              // Capture button to take a photo (optional implementation)
           }).catch(function(error) {
               console.log("Error accessing camera:", error);
           });
@@ -37,20 +30,20 @@ document.addEventListener("DOMContentLoaded", function() {
       }
   }
 
-  // QR Code Scanning
   scanBtn.addEventListener('click', function() {
+      if (!videoStream) {
+          console.log("Camera not initialized");
+          return;
+      }
       navigateToScreen(qrScreen);
       startQRScanning();
   });
 
-  // Start QR Code Scanning
   function startQRScanning() {
-      qrScanner = new jsQR();
       const canvasElement = document.createElement('canvas');
       const context = canvasElement.getContext('2d');
       qrScreen.appendChild(canvasElement);
 
-      // Scan the video stream
       function scan() {
           if (videoElement.readyState === videoElement.HAVE_ENOUGH_DATA) {
               canvasElement.height = videoElement.videoHeight;
@@ -64,7 +57,7 @@ document.addEventListener("DOMContentLoaded", function() {
                   navigateToScreen(rewardScreen);
                   stopCamera();
               } else {
-                  requestAnimationFrame(scan); // Keep scanning
+                  requestAnimationFrame(scan);
               }
           } else {
               requestAnimationFrame(scan);
@@ -74,7 +67,6 @@ document.addEventListener("DOMContentLoaded", function() {
       scan();
   }
 
-  // Stop Camera
   function stopCamera() {
       if (videoStream) {
           videoStream.getTracks().forEach(track => track.stop());
@@ -84,7 +76,6 @@ document.addEventListener("DOMContentLoaded", function() {
       }
   }
 
-  // Navigate between screens
   function navigateToScreen(screen) {
       [homeScreen, cameraScreen, qrScreen, rewardScreen].forEach(s => s.classList.add('hidden'));
       screen.classList.remove('hidden');
