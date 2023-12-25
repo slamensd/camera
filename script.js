ed1c7f79e76754207fae398001227054fbfcae4a
slamensd <58567346+slamensd@users.noreply.github.com>
2023-12-25 19:03:44 +0000

    go

--- script.js
+++ script.js
@@ -1,50 +1,92 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const screen1 = document.getElementById('screen1');
-  const screen2 = document.getElementById('screen2');
-  const screen3 = document.getElementById('screen3');
-  const cameraStream = document.getElementById('cameraStream');
-  const qrCameraStream = document.getElementById('qrCameraStream');
-  const captureBtn = document.getElementById('captureBtn');
-  const scanQRBtn = document.getElementById('scanQRBtn');
+document.addEventListener("DOMContentLoaded", function() {
+  // DOM elements
+  const captureBtn = document.getElementById('capture-btn');
+  const scanBtn = document.getElementById('scan-btn');
+  const homeScreen = document.getElementById('home-screen');
+  const cameraScreen = document.getElementById('camera-screen');
+  const qrScreen = document.getElementById('qr-screen');
+  const rewardScreen = document.getElementById('reward-screen');
+  let videoStream = null;
+  let videoElement = null;
+  let qrScanner = null;
 
-  function showScreen(screenElement) {
-    screen1.style.display = 'none';
-    screen2.style.display = 'none';
-    screen3.style.display = 'none';
-    screenElement.style.display = 'block';
-  }
-
-  function startCamera(stream, targetVideoElement) {
-    targetVideoElement.srcObject = stream;
-    targetVideoElement.play();
-    showScreen(screen2);
-  }
-
-  function startQRScanner(stream) {
-    startCamera(stream, qrCameraStream);
-  }
-
-  captureBtn.addEventListener('click', () => {
-    navigator.mediaDevices
-      .getUserMedia({ video: { facingMode: 'environment' } })
-      .then((stream) => {
-        startCamera(stream, cameraStream);
-      })
-      .catch((err) => {
-        console.error('Camera access error:', err);
-        alert('Error accessing the camera. Please make sure you have given the necessary permissions.');
-      });
+  // Navigate to Camera Screen
+  captureBtn.addEventListener('click', function() {
+      navigateToScreen(cameraScreen);
+      initializeCamera();
   });
 
-  scanQRBtn.addEventListener('click', () => {
-    navigator.mediaDevices
-      .getUserMedia({ video: { facingMode: 'environment' } })
-      .then((stream) => {
-        startQRScanner(stream);
-      })
-      .catch((err) => {
-        console.error('Camera access error:', err);
-        alert('Error accessing the camera. Please make sure you have given the necessary permissions.');
-      });
+  // Initialize Camera
+  function initializeCamera() {
+      if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+          const constraints = { video: { facingMode: "environment" } };
+
+          navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+              videoStream = stream;
+              videoElement = document.createElement('video');
+              videoElement.srcObject = stream;
+              videoElement.play();
+              cameraScreen.appendChild(videoElement);
+
+              // Capture button to take a photo (optional implementation)
+          }).catch(function(error) {
+              console.log("Error accessing camera:", error);
+          });
+      } else {
+          console.log("Camera not supported");
+      }
+  }
+
+  // QR Code Scanning
+  scanBtn.addEventListener('click', function() {
+      navigateToScreen(qrScreen);
+      startQRScanning();
   });
+
+  // Start QR Code Scanning
+  function startQRScanning() {
+      qrScanner = new jsQR();
+      const canvasElement = document.createElement('canvas');
+      const context = canvasElement.getContext('2d');
+      qrScreen.appendChild(canvasElement);
+
+      // Scan the video stream
+      function scan() {
+          if (videoElement.readyState === videoElement.HAVE_ENOUGH_DATA) {
+              canvasElement.height = videoElement.videoHeight;
+              canvasElement.width = videoElement.videoWidth;
+              context.drawImage(videoElement, 0, 0, canvasElement.width, canvasElement.height);
+              const imageData = context.getImageData(0, 0, canvasElement.width, canvasElement.height);
+              const code = jsQR(imageData.data, imageData.width, imageData.height, { inversionAttempts: "dontInvert" });
+
+              if (code) {
+                  console.log("QR Code found: ", code.data);
+                  navigateToScreen(rewardScreen);
+                  stopCamera();
+              } else {
+                  requestAnimationFrame(scan); // Keep scanning
+              }
+          } else {
+              requestAnimationFrame(scan);
+          }
+      }
+
+      scan();
+  }
+
+  // Stop Camera
+  function stopCamera() {
+      if (videoStream) {
+          videoStream.getTracks().forEach(track => track.stop());
+      }
+      if (videoElement) {
+          videoElement.remove();
+      }
+  }
+
+  // Navigate between screens
+  function navigateToScreen(screen) {
+      [homeScreen, cameraScreen, qrScreen, rewardScreen].forEach(s => s.classList.add('hidden'));
+      screen.classList.remove('hidden');
+  }
 });
